ccd6284cc69d7a841d3b6075f16c7a2a5448ab28
Fortes, Luis - Dell Team <L_Fortes@DellTeam.com>
2019-08-02 16:58:29 -0500

    initial commit

--- src/app/index.js
+++ src/app/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Clock from './components/clock';
 
 function App() {
   return (
+    // <Clock />
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <Clock />
+
         <a
           className="App-link"
           href="https://reactjs.org"
